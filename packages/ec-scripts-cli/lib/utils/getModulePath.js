e635a69d206cdf6bdefbbd0fbed85d6d54f93b98
Felix Laukel <f.laukel@ecentral.de>
2019-02-14 15:05:10 +0100

    Fix wrong package resolution in ec-cli

--- packages/ec-scripts-cli/lib/utils/getModulePath.js
+++ packages/ec-scripts-cli/lib/utils/getModulePath.js
@@ -2,7 +2,11 @@ const path = require('path');
 const settings = require('../settings');
 
 const getModulePath = (pkgName) => (
-    require.resolve(pkgName, { paths: [settings.appPath] })
+    // require.resolve refers to the `main` entry in package.json be default.
+    // Therefore we require the package.json itself and then use dirname.
+    path.dirname(
+        require.resolve(`${pkgName}/package.json`, { paths: [settings.appPath] })
+    )
 );
 
 module.exports = getModulePath;
